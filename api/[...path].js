17718867993bf618933df4d6a40ae4b4e768a7ee
Armie Miranda <admiranda@sscgi.com>
2026-02-03 11:55:16 +0800

    for deployment

--- api/[...path].js
+++ api/[...path].js
@@ -1,7 +1,3 @@
-// Vercel Serverless Function (Node.js)
-// Proxies all `/api/*` requests to the Express app in `server/`.
-
+// Vercel Serverless Function - proxies all /api/* to Express app
 const app = require('../server/app');
-
 module.exports = (req, res) => app(req, res);
-
