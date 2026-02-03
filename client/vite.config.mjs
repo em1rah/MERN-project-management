ddd3c690fc06cc595c0513dd9815e9b9549be42c
Armie Miranda <admiranda@sscgi.com>
2026-02-03 10:48:24 +0800

    for deployment

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -8,9 +8,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: path.resolve(__dirname, '../dist'),
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
